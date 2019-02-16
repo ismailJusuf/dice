8a1811f923d02eac3ff23ffc42625d2247157527
ismailJusuf <ismailyoussef44@gmail.com>
2019-02-16 14:49:11 +0300

    done

--- app/src/main/java/com/example/zar/MainActivity.java
+++ app/src/main/java/com/example/zar/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.zar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView zar;
+
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        zar = findViewById(R.id.image_view_dice);
+
+        zar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
+    }
+
+    private void rollDice() {
+
+        int randomNumber = random.nextInt(6) + 1;
+
+        switch (randomNumber) {
+            case 1:
+                zar.setImageResource(R.drawable.zar1);
+                break;
+            case 2:
+                zar.setImageResource(R.drawable.zar2);
+                break;
+            case 3:
+                zar.setImageResource(R.drawable.zar3);
+                break;
+            case 4:
+                zar.setImageResource(R.drawable.zar4);
+                break;
+            case 5:
+                zar.setImageResource(R.drawable.zar5);
+                break;
+            case 6:
+                zar.setImageResource(R.drawable.zar6);
+                break;
+            default:
+                zar.setImageResource(R.drawable.zar1);
+                break;
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/zar/logIn.java
+++ app/src/main/java/com/example/zar/logIn.java
@@ -1,13 +1,59 @@
 package com.example.zar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class logIn extends AppCompatActivity {
+
+    EditText email,password;
+    Button login;
+
+    String emailS, passwordS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        login = findViewById(R.id.login_button);
+
+        email = findViewById(R.id.edit_text_email);
+        password = findViewById(R.id.password_edit_text);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                emailS = email.getText().toString();
+                passwordS = password.getText().toString();
+
+                if (emailS == "ismail@gmail.com"){
+
+                    if (passwordS == "123"){
+
+                    } else {
+                        Toast.makeText(logIn.this, "şifrenizi yanliş girdiniz", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    //Toast.makeText(logIn.this, "emailinizi kontrol edin", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(logIn.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
